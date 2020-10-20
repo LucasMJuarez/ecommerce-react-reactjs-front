90899805dbdb91406139fb8c7bcbe29c9a3fc972
LucasMJuarez <lumas89@gmail.com>
2020-10-19 21:04:25 -0300

    first commit frontend

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import CartScreen from "./Screens/CartSreen";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
+import ProductsScreen from "./Screens/ProductsScreen";
+import RegisterScreen from "./Screens/RegisterScreen";
+import SigninScreen from "./Screens/SigninScreen";
+import ShippingScreen from './Screens/ShippingScreen'
+import PaymentScreen from "./Screens/PaymentScreen";
+import PlaceOrderScreen from "./Screens/PlaceOrderScreen";
+
+function App() {
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  };
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  };
+
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>☰</button>
+            <Link to="/">Mercado Pibe</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            {userInfo ? (
+              <Link to="/profile">{userInfo.name}</Link>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            X
+          </button>
+          <ul>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
+            <li>
+              <a href="index.html">Shirts</a>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/products" component={ProductsScreen} />
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/payment" component={PaymentScreen} />
+            <Route path="/placeorder" component={PlaceOrderScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+             <Route exact={true} path="/" component={HomeScreen} />
+          </div>
+        </main>
+        <footer className="footer">All right reserved</footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
